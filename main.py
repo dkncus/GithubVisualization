7da243b0c53e6f39eb4ad3a7431c64cec3f7281d
dkncus <dkncus@gmail.com>
2020-12-01 11:34:49 -0500

    Debugging Heroku

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import streamlit as st
 def create_visualizations():
     try:
         st.subheader("Fetching diagnostics and commit history on this repository...")
+        print("Attempting Authentication to Github API")
         g = GithubVis(user, repo, load_from_csv=False, save_csv=False)
+        print("Github API Authentication Successful.")
         commits_over_time = g.visualize_commits_over_time(show_authors=True)
         commits_over_time.figure.savefig("commits.png")
         changes_over_time = g.visualize_changes_over_time(show_authors=True)
